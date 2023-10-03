ce0fa984c0fcb981b2d23995b68e1f811bf66942
AstafevaAnastasia <127863893+AstafevaAnastasia@users.noreply.github.com>
2023-10-03 15:37:40 +0400

    Создание класса функции y=cos(x), реализующего интерфейс MathFunction

---
+++ Project2/functions/java/CosineFunction.java
@@ -0,0 +1,7 @@
+public class CosineFunction implements MathFunction{
+    @Override
+    public double apply(double x) {
+        // Реализация функции y = cos(x)
+        return Math.cos(x);
+    }
+}
