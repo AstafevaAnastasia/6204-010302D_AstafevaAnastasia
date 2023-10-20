cf1594c795365f262300df219a65656f8b1e32f7
Ivanova <dariashikotan@gmail.com>
2023-10-20 22:19:21 +0300

    создала отдельные классы для ZeroFunction и UnitFunction и написала для них тесты

---
+++ Project2/functions/java/packFunctions/UnitFunction.java
@@ -0,0 +1,7 @@
+package packFunctions;
+
+public class UnitFunction extends ConstantFunction {
+    public UnitFunction() {
+        super(1);
+    } // вызываем конструктор суперкласса со значением 1
+}
---
+++ Project2/functions/java/packFunctions/ZeroFunction.java
@@ -0,0 +1,7 @@
+package packFunctions;
+
+public class ZeroFunction extends ConstantFunction {
+    public ZeroFunction() {
+        super(0);
+    } // вызываем конструктор суперкласса со значением 0, реализуя тем самым класс функций, возвращающих ноль
+}
