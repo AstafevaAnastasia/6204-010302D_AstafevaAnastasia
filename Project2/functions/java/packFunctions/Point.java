c1051baa399f74d7dbaca05da1088ad987d2d9c3
Ivanova <dariashikotan@gmail.com>
2023-10-31 16:51:15 +0300

    Point class

---
+++ Project2/functions/java/packFunctions/Point.java
@@ -0,0 +1,12 @@
+package packFunctions;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double newX, double newY) {
+        this.x = newX;
+        this.y = newY;
+    }
+}
+
