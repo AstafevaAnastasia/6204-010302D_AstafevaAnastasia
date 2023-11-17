b3038f665de1c669b959572181454314c794b8b0
Ivanova <dariashikotan@gmail.com>
2023-11-17 22:40:14 +0300

    serialize + out

--- Project2/functions/java/io/FunctionsIO.java
+++ Project2/functions/java/io/FunctionsIO.java
@@ -1,16 +1,21 @@
 package io;
 import packFunctions.TabulatedFunction;
-import java.io.BufferedWriter;
+import java.io.*;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.BufferedReader;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 import packFunctions.factory.TabulatedFunctionFactory;
+
 public final class FunctionsIO {
     private FunctionsIO() throws UnsupportedOperationException {
         throw new UnsupportedOperationException("Instances of FunctionsIO are not allowed");
     }
 
-    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
+    public static void writeTabulatedFunction(BufferedOutputStream writer, TabulatedFunction function) throws IOException {
         int pointCount = function.getCount();
         PrintWriter printWriter = new PrintWriter(writer);
 
@@ -27,4 +32,44 @@ public final class FunctionsIO {
         printWriter.flush();
     }
 
+    public static void serialize(BufferedOutputStream stream, TabulatedFunction function) throws IOException{
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(stream);
+        objectOutputStream.writeObject(function);
+        objectOutputStream.flush();
+    }
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        String line = reader.readLine();
+        int pointCount = Integer.parseInt(line);
+
+        double[] xValues = new double[pointCount];
+        double[] yValues = new double[pointCount];
+
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+
+        for (int i = 0; i < pointCount; i++) {
+            line = reader.readLine();
+            String[] values = line.split(" ");
+            try {
+                xValues[i] = numberFormat.parse(values[0]).doubleValue();
+                yValues[i] = numberFormat.parse(values[1]).doubleValue();
+            } catch (ParseException e) {
+                throw new IOException("Error parsing values from the file.", e);
+            }
+        }
+
+        return factory.create(xValues, yValues);
+
+    }
+
+    public static Object deserialize(BufferedInputStream inputStream) {
+        Object obj = null;
+        try {
+            ObjectInputStream in = new ObjectInputStream(inputStream);
+            obj = in.readObject();
+            in.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
 }
